df2a615f8b236a1e3d28115773896c039cd435cf
Austin Whittier <austin.whitt@gmail.com>
2015-04-29 15:05:06 -0700

    Initial commit

---
+++ SecretPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecretPad
+//
+//  Created by Austin Whittier on 4/29/15.
+//  Copyright (c) 2015 Austin Whittier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
